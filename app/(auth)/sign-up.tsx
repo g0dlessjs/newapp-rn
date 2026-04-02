d8311ca3cbcb89ba9f8711a4e336870b090b730b
g0dlessjs <juangalarce.dev@gmail.com>
2026-04-02 09:50:29 -0300

    feat: setup navigation structure with auth and tabs routes

---
+++ app/(auth)/sign-up.tsx
@@ -0,0 +1,12 @@
+import { Link } from "expo-router";
+import { Text, View } from "react-native";
+
+const SignUp = () => {
+  return (
+    <View>
+      <Text>SignUp</Text>
+      <Link href="/(auth)/sign-up">Sign In</Link>
+    </View>
+  );
+};
+export default SignUp;
