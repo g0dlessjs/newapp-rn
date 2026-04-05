951ce20db1b90f0a0fea27875079aa7c8f2421a3
g0dlessjs <juangalarce.dev@gmail.com>
2026-04-04 23:34:56 -0300

    feat:  project setup, nativewind, routing, tab navigation

--- app/(tabs)/index.jsx
+++ app/(tabs)/index.jsx
@@ -1,9 +1,14 @@
+import "@/global.css";
 import { Link } from "expo-router";
-import { Text, View } from "react-native";
+import { styled } from "nativewind";
+import { Text } from "react-native";
+import { SafeAreaView as RNSaveAreaView } from "react-native-safe-area-context";
+
+const SafeAreaView = styled(RNSaveAreaView);
 
 export default function App() {
   return (
-    <View className="flex-1 items-center justify-center bg-background">
+    <SafeAreaView className="flex-1 bg-background p-5">
       <Text className="text-xl font-bold text-success">
         Welcome to Nativewind!
       </Text>
@@ -26,7 +31,7 @@ export default function App() {
         Go to Sign up
       </Link>
 
-      <Link href="/subscriptions/spotify">Spotify Subscription</Link>
+      {/* <Link href="/subscriptions/spotify">Spotify Subscription</Link> */}
       <Link
         href={{
           pathname: "/subscriptions/[id]",
@@ -35,6 +40,6 @@ export default function App() {
       >
         Claude Max Subscription
       </Link>
-    </View>
+    </SafeAreaView>
   );
 }
--- app/(tabs)/insights.tsx
+++ app/(tabs)/insights.tsx
@@ -1,11 +1,14 @@
-import React from "react";
-import { Text, View } from "react-native";
+import { styled } from "nativewind";
+import { Text } from "react-native";
+import { SafeAreaView as RNSaveAreaView } from "react-native-safe-area-context";
+
+const SafeAreaView = styled(RNSaveAreaView);
 
 const insights = () => {
   return (
-    <View>
+    <SafeAreaView className="flex-1 bg-background p-5">
       <Text>insights</Text>
-    </View>
+    </SafeAreaView>
   );
 };
 
